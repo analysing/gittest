68663eb15655009a348a82c44739d8319f37b95c
bill <328427356@qq.com>
2018-11-27 12:01:12 +0800

    =

--- Rsa.php
+++ Rsa.php
@@ -5,14 +5,40 @@
  */
 class Rsa
 {
-    protected $privateKey = '';
-    protected $publicKey = '';
+    public $privateKey = '';
+    public $publicKey = '';
 
-    function __construct()
+    public function __construct()
     {
-        $resource = openssl_pkey_new();
-        openssl_pkey_export($resource, $this->privateKey);
+        $conf = ['config' => 'D:\phpStudy\PHPTutorial\php\php-5.6.27-nts\extras\openssl.cnf'];
+        $resource = openssl_pkey_new($conf) or die('<br>err：'. openssl_error_string());
+        openssl_pkey_export($resource, $this->privateKey, null, $conf);
         $detail = openssl_pkey_get_details($resource);
         $this->publicKey = $detail['key'];
     }
+
+    public function publicEncrypt($data)
+    {
+        openssl_public_encrypt($data, $crypted, $this->publicKey);
+        return $crypted;
+    }
+
+    public function publicDecrypt($data)
+    {
+        openssl_public_decrypt($data, $decrypted, $this->publicKey);
+        return $decrypted;
+    }
+
+    public function privateEncrypt($data)
+    {
+        openssl_private_encrypt($data, $crypted, $this->privateKey);
+        return $crypted;
+    }
+
+    public function privateDecrypt($data)
+    {
+        openssl_private_decrypt($data, $decrypted, $this->privateKey);
+        return $decrypted;
+    }
 }
+
